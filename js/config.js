29453120f368a0e5ff94bf811cb17189199f8de6
hrushig <hrushig@gmail.com>
2019-04-11 15:16:53 -0700

    adding default serverless project

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_pOOAx4DOI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '77t3lb9peqsc11t12og3q77qh5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://api.warriors-hg.co.uk/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
